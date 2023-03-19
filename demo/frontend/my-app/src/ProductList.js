c39fb7940f2cfd73810c44a91a08a75715962539
Pop Raul <luarpop@yahoo.com>
2023-03-19 18:28:57 +0200

    commit 2

--- demo/frontend/my-app/src/ProductList.js
+++ demo/frontend/my-app/src/ProductList.js
@@ -3,10 +3,16 @@ import "./useless/index.css";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Button} from "react-bootstrap";
 import {useNavigate} from "react-router-dom";
+import InputGroup from 'react-bootstrap/InputGroup';
+import Form from 'react-bootstrap/Form';
 
 export function ProductList(){
 
     const [products, setProducts] = useState([]);
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState('');
+    const [category, setCategory] = useState('');
     let navigate = useNavigate();
 
     useEffect(() => {
@@ -24,8 +30,6 @@ export function ProductList(){
         navigate(path, {state:{id:productData.id, name: productData.name, desc: productData.description}});
     }
 
-    //adauga produsul Salata Cesar o singura data
-    //trebuie un niste fielduri pentru datele unui produs nou care se va introduce
     const addProduct = () =>{
         fetch('http://localhost:8080/products', {
             method: 'POST',
@@ -33,10 +37,10 @@ export function ProductList(){
               'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                "name": "Salata Caesar",
-                "description": "ingrediente",
-                "price": "27",
-                "category": "BUC"
+                "name": name,
+                "description": description,
+                "price": price,
+                "category": category
             })
         })
             .then(res => res.json())
@@ -49,26 +53,66 @@ export function ProductList(){
         let path = 'http://localhost:8080/products/' + id;
 
         fetch(path, {
-            method: 'DELETE',
+            method: 'DELETE'
         })
             .then(res => res.json())
-            .then(data => setProducts(data))
+            .then(res => console.log(res))
     }
 
     return (
         <>
-            <div>
-                <Button variant="primary" onClick={addProduct}>Add</Button>{' '}
-            </div>
             <div className="main-container">
                 {products.map((product) => {
                     return <div>
-                        <div className="small-container-green" onClick={() => handleRoute(product)}>
-                            {product.name}
+                        <div className="small-container-green">
+                            <p onClick={() => handleRoute(product)}>{product.name}</p>
+                            <Button variant="primary" onClick={() => deleteProduct(product.id)}>Delete</Button>{' '}
                         </div>
-                        <Button variant="primary" onClick={() => deleteProduct(product.id)}>Delete</Button>{' '}
                     </div>
                 })}
+                <div className="add-container">
+                    <Button variant="primary" onClick={addProduct}>Add</Button>{' '}
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+                        <Form.Control
+                            placeholder="Name"
+                            aria-label="Name"
+                            aria-describedby="basic-addon1"
+                            value={name}
+                            onChange={e => setName(e.target.value)}
+                        />
+                    </InputGroup>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+                        <Form.Control
+                            placeholder="Description"
+                            aria-label="Description"
+                            aria-describedby="basic-addon1"
+                            value={description}
+                            onChange={e => setDescription(e.target.value)}
+                        />
+                    </InputGroup>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+                        <Form.Control
+                            placeholder="Price"
+                            aria-label="Price"
+                            aria-describedby="basic-addon1"
+                            value={price}
+                            onChange={e => setPrice(e.target.value)}
+                        />
+                    </InputGroup>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+                        <Form.Control
+                            placeholder="Category"
+                            aria-label="Category"
+                            aria-describedby="basic-addon1"
+                            value={category}
+                            onChange={e => (setCategory(e.target.value))}
+                        />
+                    </InputGroup>
+                </div>
             </div>
         </>
     );
